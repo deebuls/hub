3fae01dfe1eddaf80444840164bd2bd815a53f46
dnair2m <deebul.nair@h-brs.de>
2018-06-06 13:49:35 +0200

    [confussion matrix]

--- examples/image_retraining/retrain.py
+++ examples/image_retraining/retrain.py
@@ -826,37 +826,40 @@ def add_evaluation_step(result_tensor, ground_truth_tensor):
 
 
 def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.BuGn):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=90)
-    plt.yticks(tick_marks, classes)
+                        normalize=False,
+                        title='Confusion matrix',
+                        cmap=plt.cm.BuGn):
+  """
+  This function prints and plots the confusion matrix.
+  Normalization can be applied by setting `normalize=True`.
+  """
+  # Plot non-normalized confusion matrix
+  plt.figure(figsize=(8,8))
+  plt.imshow(cm, interpolation='nearest', cmap=cmap)
+  plt.title(title)
+  plt.colorbar()
+  tick_marks = np.arange(len(classes))
+  plt.xticks(tick_marks, classes, rotation=90)
+  plt.yticks(tick_marks, classes)
 
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
+  if normalize:
+      cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+      print("Normalized confusion matrix")
+  else:
+      print('Confusion matrix, without normalization')
 
-    print(cm)
+  print(cm)
 
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, cm[i, j],
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
+  thresh = cm.max() / 2.
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+      plt.text(j, i, cm[i, j],
+               horizontalalignment="center",
+               color="white" if cm[i, j] > thresh else "black")
 
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
+  plt.tight_layout()
+  plt.ylabel('True label')
+  plt.xlabel('Predicted label')
+  plt.savefig('/tmp/confusion_matrix.jpg')
 
 
 def run_final_eval(train_session, module_spec, class_count, image_lists,
@@ -902,11 +905,8 @@ def run_final_eval(train_session, module_spec, class_count, image_lists,
   
   cnf_matrix = confusion_matrix(test_ground_truth, predictions)
   
-  # Plot non-normalized confusion matrix
-  plt.figure(figsize=(8,8))
   plot_confusion_matrix(cnf_matrix, list(image_lists.keys()))
   plt.show()
-  plt.savefig('/tmp/confusion_matrix.jpg')
 
 def build_eval_session(module_spec, class_count):
   """Builds an restored eval session without train operations for exporting.
